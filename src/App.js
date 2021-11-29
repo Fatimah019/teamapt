aa589f6126ce944781452f5b3238ebfe7622f700
Davies Fatimah Omowonuola <daviesfatimah97@gmail.com>
2021-11-29 04:50:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Routes from "./Components/Routes";
 import PhoneImage from "./Components/UI/phoneimage";
 import Home from "./Components/Home";
@@ -6,10 +6,18 @@ import { useLocation } from "react-router-dom";
 
 function App() {
   const location = useLocation();
+  const [windowWidth, setWindowWidth] = useState("")
+  
+  useEffect (()=>{
+      if(window.innerWidth < 900){
+        setWindowWidth(<h1>Page Cannot Be Viewed On Screens Less Than 90px</h1>)
+      }
+  },[windowWidth])
 
   const checkPhone = location.hash === "#welcome" ? "phone-container-visible" : "phone-container-invisible";
   return (
     <div className="main-home">
+      {windowWidth}
       <div className={checkPhone}>
         <PhoneImage />
       </div>
