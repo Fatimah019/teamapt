35a881f3c6de3ee85a6513060f1036a0c5ef81de
Fatimah <daviesfatimah97@gmail.com>
2021-11-28 23:03:45 -0800

    pull update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routes from "./Components/Routes";
 import PhoneImage from "./Components/UI/phoneimage";
 import Home from "./Components/Home";
@@ -8,6 +8,17 @@ function App() {
   const location = useLocation();
 
   const checkPhone = location.hash === "#welcome" ? "phone-container-visible" : "phone-container-invisible";
+
+  useEffect(() => {
+    if (window.screen.width < 900) {
+      console.log("sss");
+      // return (
+      //   <div>
+      //     <h1>Cannot View Website on Screens less than 900px</h1>
+      //   </div>
+      // );
+    }
+  }, []);
   return (
     <div className="main-home">
       <div className={checkPhone}>
